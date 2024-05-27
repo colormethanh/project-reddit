e6ef3c59495b66f0751a0aa9447c4d54482fe345
Thanh Nguyen <thnguyen1847@gmail.com>
2024-05-27 16:29:25 -0400

    Fixed vertical line to be responsive

--- main.js
+++ main.js
@@ -1,18 +1,63 @@
 console.log("let's code!")
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+import {SocialMedia} from "../MyClasses.js";
+let ReReddit;
 
 
-class SocialMedia {
-  posts;
-  constructor() {
-    this.posts = [];
+$(document).ready(() => {
+  console.log("Document Ready");
+  ReReddit = new SocialMedia();
+
+  const initPage = function() {
+    ReReddit.addPost("ColorMeThanh", "Gyat! this is a post used for testing.");
+    ReReddit.addPost("Sophia", "This is sophia writing a post for testing as well.");
+    const postArray = ReReddit.getPosts()
+    postArray.forEach(post => {
+      console.log(post.getPost());
+    });
+  };
+  initPage();
+
+  const createPostElement = function() {
+   const newElement =  $("<h1 />", {
+      html: "This is a new post",
+      "class": "row"
+    });
+
+    return newElement;
+  };
+
+  const addPostToPage = function(element) {
+    element.appendTo("#posts-inner");
   }
+
+  addPostToPage(createPostElement());
+  addPostToPage(createPostElement());
+
+  // like/dislike button functionality
+  $(".like-btn").click((event) => {
+    console.log("like button clicked");
+    console.log(`Type is: ${event.currentTarget.dataset.type}`);
+  });
+
+  // Comment button functionality
+  $(".comment-btn").click(() => {
+    console.log("comment button clicked");
+  });
   
-  createPost(author, text) {
-    
-  }
+  // Submit Post functionality 
+  $("#create-post-form").on("submit", (event) => {
+    event.preventDefault()
+    console.log("post has submitted");
+  });
+  
+  // Submit comment functionality
+  // why does it submit twice??
+  $(".comment-form").on("submit", (event) => {
+    event.preventDefault();
+    console.log("Comment form submitted");
+  });
 
-  addPost(postData){
 
-  }
-}
+
+
+})
